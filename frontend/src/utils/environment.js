8d7489250954d8c287e1e2cca8fd34fbdfe4b9c5
Richard <richardfehr0226@gmail.com>
2026-09-14 09:27:33 +0300

    updated 3  firebase environmrnt.js

--- frontend/src/utils/environment.js
+++ frontend/src/utils/environment.js
@@ -15,11 +15,11 @@ requiredVariables.forEach((key) => {
 });
 
 export const env = {
-  firebaseApiKey: import.meta.env.FIREBASE_API_KEY,
-  firebaseAuthDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
-  firebaseProjectId: import.meta.env.FIREBASE_PROJECT_ID,
-  firebaseStorageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
-  firebaseMessagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
-  firebaseAppId: import.meta.env.FIREBASE_APP_ID,
-  firebaseMeasurementId: import.meta.env.FIREBASE_MEASUREMENT_ID,
+  firebaseApiKey: __FIREBASE_API_KEY__,
+  firebaseAuthDomain: __FIREBASE_AUTH_DOMAIN__,
+  firebaseProjectId: __FIREBASE_PROJECT_ID__,
+  firebaseStorageBucket: __FIREBASE_STORAGE_BUCKET__,
+  firebaseMessagingSenderId: __FIREBASE_MESSAGING_SENDER_ID__,
+  firebaseAppId: __FIREBASE_APP_ID__,
+  firebaseMeasurementId: __FIREBASE_MEASUREMENT_ID__,
 };
