76176cc2d3ce4af28b654379c469f64ef61455f6
Richard <richardfehr0226@gmail.com>
2026-09-14 09:24:05 +0300

    updated 2  firebase environmrnt.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,4 +8,14 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
   },
+
+  define: {
+    __FIREBASE_API_KEY__: JSON.stringify(process.env.FIREBASE_API_KEY),
+    __FIREBASE_AUTH_DOMAIN__: JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+    __FIREBASE_PROJECT_ID__: JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+    __FIREBASE_STORAGE_BUCKET__: JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+    __FIREBASE_MESSAGING_SENDER_ID__: JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+    __FIREBASE_APP_ID__: JSON.stringify(process.env.FIREBASE_APP_ID),
+    __FIREBASE_MEASUREMENT_ID__: JSON.stringify(process.env.FIREBASE_MEASUREMENT_ID),
+  },
 })
