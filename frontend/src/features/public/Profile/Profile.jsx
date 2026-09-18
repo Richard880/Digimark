fbcaf738ed25e982361ccd21d82f282fe706df29
Sir Richard Fehr <125535291+Richard880@users.noreply.github.com>
2026-09-18 13:16:11 +0300

    Update Profile.jsx

--- frontend/src/features/public/Profile/Profile.jsx
+++ frontend/src/features/public/Profile/Profile.jsx
@@ -7,6 +7,7 @@ import LegDistributionCards from "../../vendor-dashboard/LegDistributionCards";
 import AvatarMenuModal from "./AvatarMenuModal";
 import { uploadImageToCloudinary } from "../../../utils/cloudinaryUploader";
 
+
 const API_URL =
   import.meta.env.VITE_API_URL || "http://localhost:3000";
 
