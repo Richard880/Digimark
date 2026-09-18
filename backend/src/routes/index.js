9289b1c2dee5574c4ac26d6ec1f362189af9eb8a
Sir Richard Fehr <125535291+Richard880@users.noreply.github.com>
2026-09-18 11:39:47 +0300

    Update index.js

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,5 +4,7 @@ const router = express.Router();
 router.use("/auth", require("../modules/auth/auth.routes"));
 router.use("/profiles", require("../modules/profiles/profile.routes"));
 router.use("/products", require("../modules/products/product.routes"));
+router.use("/upload",require("./upload.routes"));
+
 
 module.exports = router;
