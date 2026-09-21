5bddbe91fd95cb301ab6be5b81ffaf843b78448d
Sir Richard Fehr <125535291+Richard880@users.noreply.github.com>
2026-09-21 12:10:03 +0300

    Update MyTeamDirectory.jsx

--- frontend/src/features/vendor-dashboard/MyTeamDirectory.jsx
+++ frontend/src/features/vendor-dashboard/MyTeamDirectory.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import useAuth from "../../auth/hooks/useAuth";
+import useAuth from "../auth/hooks/useAuth";
 
 const API_URL = import.meta.env.PROD 
   ? "" 
