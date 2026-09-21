460e879682e0ce1e7f8429a0f58928d41ea7fa4b
Sir Richard Fehr <125535291+Richard880@users.noreply.github.com>
2026-09-21 06:57:10 +0300

    Update auth.service.js

--- backend/src/modules/auth/auth.service.js
+++ backend/src/modules/auth/auth.service.js
@@ -1,5 +1,5 @@
 const User = require("../../models/User");
-const UserProfile = require("../../models/UserProfile");
+const UserProfile = require("../../models/UserProfile.js");
 const matrixService = require("../../../services/matrixService");
 const generateMembershipNumber = require("../../utils/membershipNumber");
 
