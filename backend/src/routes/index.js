20c37f52ab30896d13ee0ec2265e2aa3ed016b2f
Sir Richard Fehr <125535291+Richard880@users.noreply.github.com>
2026-09-22 08:02:41 +0300

    Update index.js

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -6,5 +6,6 @@ router.use("/profiles", require("../modules/profiles/profile.routes"));
 router.use("/products", require("../modules/products/product.routes"));
 router.use("/upload",require("./upload.routes"));
 router.use("/network",require("../../api/network.routes")); // Adjust path to network.routes.js
+router.use("/orders", require("../modules/order/order.routes")); 
 
 module.exports = router;
